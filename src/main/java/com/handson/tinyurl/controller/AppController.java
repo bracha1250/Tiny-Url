0adb1c2c460f221a00df78fe6a3392218330379c
root <brachi1770@gmail.com>
2023-01-03 13:49:59 +0200

    commit - with mongo

--- src/main/java/com/handson/tinyurl/controller/AppController.java
+++ src/main/java/com/handson/tinyurl/controller/AppController.java
@@ -1,23 +1,100 @@
 package com.handson.tinyurl.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.handson.tinyurl.model.NewTinyRequest;
+import com.handson.tinyurl.model.User;
+import com.handson.tinyurl.repository.UserRepository;
 import com.handson.tinyurl.service.Redis;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Random;
+
+import static com.handson.tinyurl.model.User.UserBuilder.anUser;
+import static com.handson.tinyurl.util.Dates.getCurMonth;
+import static org.springframework.data.mongodb.core.query.Criteria.*;
 
 @RestController
 public class AppController {
 
     @Autowired
+    ObjectMapper om;
+    @Autowired
     Redis redis;
+    @Value("${base.url}")
+    String baseUrl;
 
-    @RequestMapping(value = "/getKey", method = RequestMethod.GET)
-    public String getKey(@RequestParam String key) {
-        System.out.println(redis.get(key).toString());
-        return redis.get(key).toString();
+    Random random=new Random();
+private static final int MAX_RETRIES=4;
+private static final int TINY_LENGTH = 6;
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    @RequestMapping(value = "/user", method = RequestMethod.POST)
+    public User createUser(@RequestParam String name) {
+        User user = anUser().withName(name).build();
+        user = userRepository.insert(user);
+        return user;
     }
-    @RequestMapping(value = "/setKey", method = RequestMethod.GET)
-    public Boolean setKey(@RequestParam String key,@RequestParam String value) {
 
-        return redis.set(key,value);
+    @RequestMapping(value = "/user/{name}", method = RequestMethod.GET)
+    public User getUser(@RequestParam String name) {
+        User user = userRepository.findFirstByName(name);
+        return user;
+    }
+
+    private void incrementMongoField(String userName, String key){
+        Query query = Query.query(where("name").is(userName));
+        Update update = new Update().inc(key, 1);
+        mongoTemplate.updateFirst(query, update, "users");
+    }
+
+
+    @RequestMapping(value = "/tiny", method = RequestMethod.POST)
+    public String generate(@RequestBody NewTinyRequest request) throws JsonProcessingException {
+        String tinyCode = generateTinyCode();
+        int i = 0;
+        while (!redis.set(tinyCode, om.writeValueAsString(request)) && i < MAX_RETRIES) {
+            tinyCode = generateTinyCode();
+            i++;
+        }
+        if (i == MAX_RETRIES) throw new RuntimeException("SPACE IS FULL");
+        return baseUrl + tinyCode + "/";
+    }
+
+    @RequestMapping(value = "/{tiny}/", method = RequestMethod.GET)
+    public ModelAndView getTiny(@PathVariable String tiny) throws JsonProcessingException {
+        Object tinyRequestStr = redis.get(tiny);
+        NewTinyRequest tinyRequest = om.readValue(tinyRequestStr.toString(),NewTinyRequest.class);
+        if (tinyRequest.getLongUrl() != null) {
+            String userName = tinyRequest.getUserName();
+            if ( userName != null) {
+                incrementMongoField(userName, "allUrlClicks");
+                incrementMongoField(userName,
+                        "shorts."  + tiny + ".clicks." + getCurMonth());
+            }
+            return new ModelAndView("redirect:" + tinyRequest.getLongUrl());
+        } else {
+            throw new RuntimeException(tiny + " not found");
+        }
+    }
+    private String generateTinyCode() {
+        String charPool = "ABCDEFHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < TINY_LENGTH; i++) {
+
+            res.append(charPool.charAt(random.nextInt(charPool.length())));
+        }
+        return res.toString();
     }
 }
